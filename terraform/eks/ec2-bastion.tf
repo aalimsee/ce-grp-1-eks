295b3246880a138294cbc486bf3ebfb095ea5194
aaron lim <aaron.limse@hotmail.com>
2025-05-25 16:09:39 +0800

    test: terraform-eks.yml actions

--- terraform/eks/ec2-bastion.tf
+++ terraform/eks/ec2-bastion.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
