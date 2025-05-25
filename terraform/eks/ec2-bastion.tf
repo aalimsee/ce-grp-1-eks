dd48b36527272c9bcce1b63d5e34a76458fb93bf
aaron lim <aaron.limse@hotmail.com>
2025-05-25 16:51:47 +0800

    test: my_public_ip

--- terraform/eks/ec2-bastion.tf
+++ terraform/eks/ec2-bastion.tf
@@ -1,5 +1,5 @@
-data "http" "my_ip" {
-  url = "https://checkip.amazonaws.com/"
+data "http" "my_public_ip" {
+  url = "https://ifconfig.me/ip"
 }
 
 resource "aws_security_group" "bastion_sg" {
@@ -12,7 +12,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
+    cidr_blocks = ["${chomp(data.http.my_public_ip.response_body)}/32"]
   }
 
   egress {
