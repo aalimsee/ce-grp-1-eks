1ca5434b507377165bbfe8f8d3fc3e68f5ba05d2
aaron lim <aaron.limse@hotmail.com>
2025-05-26 02:18:18 +0800

    change version = 20.36.0

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -2,6 +2,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# # Force Terraform to Use v19.21.0
+# terraform {
+#   required_version = ">= 1.0"
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 4.57"
+#     }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = ">= 2.10"
+#     }
+#     tls = {
+#       source  = "hashicorp/tls"
+#       version = ">= 3.0"
+#     }
+#     time = {
+#       source  = "hashicorp/time"
+#       version = ">= 0.9"
+#     }
+#   }
+# }
+
 # Remote state to pull VPC values
 # The terraform_remote_state data source only needs to read the state file — it doesn’t lock it or write to it.
 # So it doesn't need DynamoDB table info (no use_lock_table, dynamodb_table, etc.).
@@ -18,9 +42,10 @@ data "terraform_remote_state" "vpc" {
 
 # EKS Cluster
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.36.0"
   # version = "20.8.4"
-  version = "19.21.0" # ✅ Supports aws_auth_users and manage_aws_auth
+  # version = "19.21.0" # ✅ Supports aws_auth_users and manage_aws_auth
 
   cluster_name    = "ce-grp-1-eks"
   cluster_version = "1.32"
@@ -48,23 +73,28 @@ module "eks" {
   #       kubernetes_groups   = ["cluster-admins"]
   #       policy_associations = []
   #     }
-  #     # Add aalimsee_ce9 to EKS Access Entries
-  #     # aalimsee-user = {
-  #     #   principal_arn       = "arn:aws:iam::255945442255:user/aalimsee_ce9"
-  #     #   kubernetes_groups   = ["cluster-admins"]
-  #     #   policy_associations = []
-  #     # }
   #   }
 
-  manage_aws_auth = true
+  authentication_mode = "API"
 
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::255945442255:user/aalimsee_ce9"
-      username = "aalimsee_ce9"
-      groups   = ["system:masters"]
+  access_entries = {
+    admin-role = {
+      principal_arn       = "arn:aws:iam::255945442255:user/aalimsee_ce9"
+      kubernetes_groups   = ["cluster-admins"]
+      policy_associations = []
     }
-  ]
+  }
+
+
+
+  #   manage_aws_auth = true
+  #   aws_auth_users = [
+  #     {
+  #       userarn  = "arn:aws:iam::255945442255:user/aalimsee_ce9"
+  #       username = "aalimsee_ce9"
+  #       groups   = ["system:masters"]
+  #     },
+  #   ]
 
 
   tags = {
