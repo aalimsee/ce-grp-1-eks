bc35ed4cc361128ca23955c29461840f2e95d4ae
aaron lim <aaron.limse@hotmail.com>
2025-06-01 20:17:39 +0800

    Creates IAM role and attaches required Route 53 permissions for prometheus and grafana

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -11,3 +11,18 @@
 #   description = "List of private subnet IDs"
 #   type        = list(string)
 # }
+
+
+# --- start
+# Accepts your EKS OIDC provider URL and ARN as variables
+
+variable "oidc_provider_url" {
+  description = "EKS cluster OIDC provider URL"
+  type        = string
+}
+
+variable "oidc_provider_arn" {
+  description = "EKS cluster OIDC provider ARN"
+  type        = string
+}
+# --- end
